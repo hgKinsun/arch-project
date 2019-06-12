4197053b2aa7d6b4353b6decc79798b6c330de20
Kin Sun <tan.kinsun@hellogold.com>
2019-06-12 13:38:00 +0800

    test commit

--- app/src/main/kotlin/com/okynk/archproject/core/util/GlobalUtil.kt
+++ app/src/main/kotlin/com/okynk/archproject/core/util/GlobalUtil.kt
@@ -14,5 +14,3 @@ fun parseInt(str: String?, defaultValue: Int = 0): Int {
 inline fun <reified T> Gson.fromJson(json: String) =
     this.fromJson<T>(json, object : TypeToken<T>() {}.type)
 
-
-//asd
