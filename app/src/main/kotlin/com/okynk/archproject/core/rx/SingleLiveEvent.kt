d9f97bfa2058e8d3451529038001b14c2cf089e3
kinsun <tan.kinsun@hellogold.com>
2020-03-04 12:30:58 +0800

    Test commit Git Action

--- app/src/main/kotlin/com/okynk/archproject/core/rx/SingleLiveEvent.kt
+++ app/src/main/kotlin/com/okynk/archproject/core/rx/SingleLiveEvent.kt
@@ -45,7 +45,7 @@ class SingleLiveEvent<T> : MutableLiveData<T>() {
             }
         })
     }
-    //asdasdasd asdasdasdasdasdasd asdasd
+    // asdasdasd asdasdasdasdasdasd asdasd
     @MainThread
     override fun setValue(t: T?) {
         pending.set(true)
